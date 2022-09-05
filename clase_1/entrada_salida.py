3c256294dc75594fea9d23299641183c9c6f16d8
Mateo L <mateoloholaberry@gmail.com>
2022-09-05 09:47:52 -0300

    Entrada y salida de datos

---
+++ clase_1/entrada_salida.py
@@ -0,0 +1,19 @@
+# Salida de datos
+print("Hola mundo")
+print('Desde visual')
+
+# Comentarios
+
+# Concatenar
+print("Hola" + "Mundo")
+
+
+# Sep
+print("Hola", "mundo", sep=" ")
+
+# End
+print("Hola", "mundo",)
+
+
+# Entrada de datos
+input("Ingrese un mensaje: ")
